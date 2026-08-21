1320d6e73472dca8e06a2634ea458db42d10e7f6
minar2495 <57590339+minar2495@users.noreply.github.com>
2026-08-21 22:47:42 +0530

    Add Insertion Sort Algorithm

--- src/Main.java
+++ src/Main.java
@@ -2,43 +2,17 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        int[] nums = { 6 , 5 , 2 , 8 , 9 , 4};
-
-        int size = nums.length;
-        int temp = 0;
-        int minIndex = -1;
-
-
-        System.out.println("Before sorting");
-        for(int num : nums){
-            System.out.print(num + "");
-        }
-
-        for (int i = 0 ; i<size-1 ;i++){
-            minIndex = i;
-            for (int j = i+1 ; j < size ; j++){
-
-                if(nums[minIndex] > nums[j] )
-                    minIndex = j;
-
-            }
-            temp = nums[minIndex];
-            nums[minIndex] = nums[i];
-            nums[i] = temp;
-
-            for(int num : nums){
-                System.out.print(num);
-            }
-            System.out.println();
-        }
-
-
+        SelectionSort selectionSort = new SelectionSort();
+        selectionSort.selectionSort();
 
         System.out.println();
-        System.out.println("After sorting");
-        for(int num : nums){
-            System.out.print(num);
-        }
+        System.out.println("----------------------");
+        System.out.println("Insertion Sorting");
+        System.out.println("----------------------");
+
+
+        InsertionSort insertionSort = new InsertionSort();
+        insertionSort.insertionSort();
     }
     //Development Branch Code
 
