facebba0e34cf3cd0338343ed0d58340bb8d578b
minar2495 <57590339+minar2495@users.noreply.github.com>
2026-08-21 23:52:58 +0530

    Add quick sort algorithm

--- src/Main.java
+++ src/Main.java
@@ -2,17 +2,26 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        SelectionSort selectionSort = new SelectionSort();
-        selectionSort.selectionSort();
+
+//        SelectionSort selectionSort = new SelectionSort();
+//        selectionSort.selectionSort();
+//
+//        System.out.println();
+//        System.out.println("----------------------");
+//        System.out.println("Insertion Sorting");
+//        System.out.println("----------------------");
+//
+//
+//        InsertionSort insertionSort = new InsertionSort();
+//        insertionSort.insertionSort();
 
         System.out.println();
         System.out.println("----------------------");
-        System.out.println("Insertion Sorting");
+        System.out.println("Quick Sorting");
         System.out.println("----------------------");
 
-
-        InsertionSort insertionSort = new InsertionSort();
-        insertionSort.insertionSort();
+        QuickSort quickSortObj = new QuickSort();
+        quickSortObj.executeMethod();
     }
     //Development Branch Code
 
