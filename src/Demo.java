92eddb3e568165f693dc39033b4092af0b5cf78b
minar2495 <57590339+minar2495@users.noreply.github.com>
2026-09-14 17:20:17 +0530

    add linked list files

---
+++ src/Demo.java
@@ -0,0 +1,9 @@
+public class Demo {
+    public static void main(String [] args){
+        int i = 5;
+        i++;
+        System.out.println(i);
+        ++i;
+        System.out.println(i);
+    }
+}
